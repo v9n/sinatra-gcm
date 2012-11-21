97172acbd58705194cdd2404c25c56e4ea1cda0d
Vinh <vinh.nguyen@acronics.com>
2012-11-20 17:08:57 -0800

    Checked existed before write

--- welcome.rb
+++ welcome.rb
@@ -108,9 +108,14 @@ get '/send_message' do
 end
 
 
-# Handling device registration. Storing registrationId into a mongo db
+# Handling device registration. Storing registrationId into a mongo db. Check if existed before inserting.
 get '/register/:id' do |id|
   #curl -X POST -H 'Content-Type:application/json' -H 'Authorization:key=AIzaSyAU1_3EdDZyKdo8oRY3vWdq3_B2iUblNGg'  -d '{"registration_ids":["APA91bE6qtP5G46xx1UIlNkocQaRpbsWt29fAldQQw8WOTvXg29-cc5q4kizOvbRsCcDobEk3vv681f545VB4PtL6lDvaME_sZs-rcD0YSyW7Q9hO5euMBEBeO0D6JidtV1R7gHvUvcrUjeslZmKzKsIKKE0-Z9bAg"],"data":{"msg":"Welcome to iCeeNee","coupon":"iCeeNee"}}' https://android.googleapis.com/gcm/send
-  device = Device.new({:reg_id => id})
-  device.save!
+  if 0==Device.where(reg_id: id).count
+    device = Device.new({:reg_id => id})
+    device.save!
+    "Saved"
+  else 
+    "existed"
+  end
 end
