c92e33da152d634466915dd5f79dd3619e17b533
Vinh <vinh.nguyen@acronics.com>
2012-11-20 16:49:05 -0800

    Use mOngolba_uri

--- welcome.rb
+++ welcome.rb
@@ -31,9 +31,9 @@ class Device
 end
 
 Mongoid.configure do |config|
-    if ENV['MONGOHQ_URL']
-      conn = Mongo::Connection.from_uri(ENV['MONGOHQ_URL'])
-      uri = URI.parse(ENV['MONGOHQ_URL'])
+    if ENV['MONGOLAB_URI']
+      conn = Mongo::Connection.from_uri(ENV['MONGOLAB_URI'])
+      uri = URI.parse(ENV['MONGOLAB_URI'])
       config.master = conn.db(uri.path.gsub(/^\//, ''))
     else
       config.master = Mongo::Connection.from_uri("mongodb://localhost:27017").db('device')
