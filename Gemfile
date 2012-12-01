c2cfe0e8a5731ca53494ed71b0343553c0081edd
Vinh <vinh.nguyen@acronics.com>
2012-11-30 16:39:26 -0800

    -Refactor UI - Message: add, remove, send - Device: register, remove

--- Gemfile
+++ Gemfile
@@ -10,3 +10,4 @@ gem 'rest-client'
 gem 'mongo'
 gem "mongoid", "~> 2.4"
 gem "bson_ext", "~> 1.5"
+gem 'rack-flash3'
--- welcome.rb
+++ welcome.rb
@@ -10,6 +10,9 @@ require 'less'
 require 'rest-client'
 require 'mongo'
 require 'mongoid'
+require 'rack-flash'
+
+use Rack::Flash
 
 class String
   def scan2(regexp)
@@ -24,20 +27,22 @@ class String
   end
 end
 
-class Device
-    include Mongoid::Document
-    
-    field :reg_id, :type => String
-end
+configure do
+  set :logging, :true
+  set :AUTHORIZE_KEY => 'AIzaSyAU1_3EdDZyKdo8oRY3vWdq3_B2iUblNGg'
+ 
+  # same as `set :option, true`
+  enable :option
 
-class Log
-  include Mongoid::Document
-    
-    field :body, :type => String
-    field :query_string, :type => String
-    field :param, :type => String 
+  # same as `set :option, false`
+  #disable :option  
+  
+  # you can also have dynamic settings with blocks
+  set(:css_dir) { File.join(views, 'css') }
+  enable :sessions  
+  
+  #Less.paths << settings.views
 end
-
 
 Mongoid.configure do |config|
     if ENV['MONGOLAB_URI']
@@ -49,22 +54,32 @@ Mongoid.configure do |config|
     end
 end
 
-configure do
-	set :logging, :true
-	set :AUTHORIZE_KEY => 'AIzaSyAU1_3EdDZyKdo8oRY3vWdq3_B2iUblNGg'
- 
-  # same as `set :option, true`
-  enable :option
-
-  # same as `set :option, false`
-  disable :option	
-	
-  # you can also have dynamic settings with blocks
-  set(:css_dir) { File.join(views, 'css') }
-	enable :sessions  
-  
-  #Less.paths << settings.views
+class Device
+    include Mongoid::Document
+    
+    field :reg_id, :type => String
+    field :os, :type => String, :default => 'android'    
 end
+
+class Log
+  include Mongoid::Document
+    
+    field :body, :type => 'String'
+    field :query_string, :type => 'String'
+    field :param, :type => 'String' 
+    field :t, :type => String, :default => 'device'
+end
+
+
+class Message
+    include Mongoid::Document
+    
+    field :body, :type => 'String'
+    field :url,  :type => 'String'
+    field :send_at, :type => Array, :default => []
+end
+
+
 
 helpers do
   include Rack::Utils
@@ -75,36 +90,68 @@ helpers do
   end
 end
 
-get '/' do		
-	haml :index, :locals => {}
+get '/' do
+  messages = Message.all
+  haml :index, :locals => {:messages => messages}  
 end
 
-get '/css/:style.css' do
-	less params[:style].to_sym, :paths => ["public/css"], :layout => false
+get '/message/remove/:id' do |id|
+  Message.find(id).delete 
+  flash[:notice] = "Message is removed!"
+  redirect '/'
 end
 
-get '/about' do
-  "@kureikain"
+post '/message/save' do
+  Log.new({body: request.body.read, query_string: request.query_string}).save
+  
+  begin 
+    message = Message.new({:body => params[:body], :url => params[:url]})
+    message.save
+    flash[:notice] = "Message saved!"
+  rescue
+    flash[:notice] = "Message cannot saved!"  
+  end
+  redirect '/'
 end
 
-get '/pag' do
-  "sasasas"
+get '/message/send' do
+  Log.new({body: request.body.read, query_string: request.query_string}).save
+
+  message = Message.new({:body => params[:body], :url => params[:url]})
+  message.save
+  redirect "/message/send/#{message.id}"
 end
 
-get '/help' do
-  "@Help Page"
+get '/message/send/:id' do |id|
+  begin 
+    m = Message.find(id)
+  rescue Mongoid::Errors::DocumentNotFound
+    flash[:notice] = "Invalid message to send"
+    redirect '/' 
+  end 
+
+  post_args = {
+    :registration_ids => ["APA91bE6qtP5G46xx1UIlNkocQaRpbsWt29fAldQQw8WOTvXg29-cc5q4kizOvbRsCcDobEk3vv681f545VB4PtL6lDvaME_sZs-rcD0YSyW7Q9hO5euMBEBeO0D6JidtV1R7gHvUvcrUjeslZmKzKsIKKE0-Z9bAg"],
+    :data => {
+      :msg    => m.body,
+      :url => m.url
+    }
+  }
+  resp = RestClient.post 'https://android.googleapis.com/gcm/send', post_args.to_json, :Authorization => 'key=' + settings.AUTHORIZE_KEY, :content_type => :json, :accept => :json
+  puts resp.inspect
+  Log.new({:body => resp.inspect, :t => 'Message Sending'})
+
+  m.send_at = [] if m.send_at.nil?
+  m.send_at.push Time.now.to_i
+  m.save  
+  puts m.inspect
+  flash[:notice] = "Message is sent to Google GCM Server."
+  redirect '/'   
 end
 
-def client
-  OAuth2::Client.new(settings.CLIENT_ID, settings.CLIENT_SECRET,
-                     :ssl => {:ca_file => '/etc/ssl/ca-bundle.pem'},
-                     :site => 'https://api.github.com',
-                     :authorize_url => 'https://github.com/login/oauth/authorize',
-                     :token_url => 'https://github.com/login/oauth/access_token')
-end
-
-
-get '/send_message' do 
+get '/send_message' do
+  Log.new({body: request.body.read, query_string: request.query_string}).save
+  
   post_args = {
     :registration_ids => ["APA91bE6qtP5G46xx1UIlNkocQaRpbsWt29fAldQQw8WOTvXg29-cc5q4kizOvbRsCcDobEk3vv681f545VB4PtL6lDvaME_sZs-rcD0YSyW7Q9hO5euMBEBeO0D6JidtV1R7gHvUvcrUjeslZmKzKsIKKE0-Z9bAg"],
     :data => {
@@ -116,8 +163,11 @@ get '/send_message' do
   resp.inspect
 end
 
+get '/device' do
+  devices = Device.all
+  haml :device, :locals => {:devices => devices}    
+end  
 
-# Handling device registration. Storing registrationId into a mongo db. Check if existed before inserting.
 get '/register/:id' do |id|
   Log.new({body: request.body.read, query_string: request.query_string}).save
   
@@ -133,13 +183,33 @@ end
 
 post '/register' do
   Log.new({param: params, body: request.body.read, query_string: request.query_string}).save
-  
+  puts params.inspect
   #curl -X POST -H 'Content-Type:application/json' -H 'Authorization:key=AIzaSyAU1_3EdDZyKdo8oRY3vWdq3_B2iUblNGg'  -d '{"registration_ids":["APA91bE6qtP5G46xx1UIlNkocQaRpbsWt29fAldQQw8WOTvXg29-cc5q4kizOvbRsCcDobEk3vv681f545VB4PtL6lDvaME_sZs-rcD0YSyW7Q9hO5euMBEBeO0D6JidtV1R7gHvUvcrUjeslZmKzKsIKKE0-Z9bAg"],"data":{"msg":"Welcome to iCeeNee","coupon":"iCeeNee"}}' https://android.googleapis.com/gcm/send
   if 0==Device.where(reg_id: params[:regId]).count
-    device = Device.new({:reg_id => params[:regId]})
+    device = Device.new({:reg_id => params[:regId], :os => params[:os].nil?? 'android':'ios'})
     device.save!
     "Saved"
   else 
     "existed"
   end
 end
+
+get '/remove_device/:name' do |name|
+  Device.find(name).delete 
+  flash[:notice] = "Device is removed!"
+  redirect '/'
+end
+
+get '/about' do
+  "@kureikain"
+end
+
+get '/help' do
+  "@Help Page"
+end
+
+get '/css/:style.css' do
+  less params[:style].to_sym, :paths => ["public/css"], :layout => false
+end
+
+
